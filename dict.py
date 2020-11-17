3c565cce0c8fb3b94274523ebd8654dae32442f6
MariskaS85 <mcfsnijdewind@gmail.com>
2020-11-17 02:25:14 +0000

    Alle works except edit needs some css

--- dict.py
+++ dict.py
@@ -5,6 +5,7 @@ from flask import (
     )
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from pygments.lexer import words
 if os.path.exists("env.py"):
     import env 
 
@@ -21,7 +22,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_words")
 def get_words():
-    words = mongo.db.words.find()
+    words = list(mongo.db.words.find())
     return render_template("home.html", words=words)
 
 @app.route("/search", methods=["GET", "POST"])
@@ -41,7 +42,6 @@ def add_words():
             "tlc": request.form.get("tlc"),
             "meaning": request.form.get("meaning"),
             "usage": request.form.get("usage"),
-            "created_on": request.form.getTimestamp("created_at"),
         }
 
         mongo.db.words.insert_one(words)
@@ -52,38 +52,20 @@ def add_words():
     return render_template("add.html", category=category)
 
 
-
-
-@app.route("/edit_words/", methods=["GET","POST"])
-def edit_words(words_id):
-    if request.method == "POST":
-        words_id = {
-            "word": request.form.get("word"),
-            "translation": request.form.get("translation"),
-            "category_name": request.form.get("category_name"),
-            "slc": request.form.get("slc"),
-            "tlc": request.form.get("tlc"),
-            "meaning": request.form.get("meaning"),
-            "usage": request.form.get("usage"),
-            "created_on": request.form.getTimestamp("created_at"),
-        }
-
-    mongo.db.words.update({"_id": ObjectId(words_id)}, submit)
-
-    word = mongo.db.words.find_one({"_id": ObjectId(words_id)})
-    words = mongo.db.words.find().sort("word", 1)
-    return render_template("edit.html", words=words)
-
-@app.route("/delete_word")
-def delete_word(words_id):
-    mongo.db.words.remove({ "_id": ObjectId(words_id)})
-    return redirect(url_for("edit_words"))
-
-
+@app.route("/edit_word/<word_id>", methods=["GET","POST"])
+def edit_word(word_id):
+    word = mongo.db.words.find_one({"_id": ObjectId(word_id)})
+    category = mongo.db.category.find().sort("category_name", 1)
+    return render_template("home.html", word=word, category=category)   
 
 @app.route("/about_page")
 def about_page():
     return render_template("about.html")
+
+@app.route("/delete_word/<word_id>")
+def delete_word(word_id):
+    mongo.db.words.remove({ "_id": ObjectId(word_id)})
+    return redirect(url_for("get_words"))
 
 if __name__=="__main__":
     app.run(host=os.environ.get("IP"),
